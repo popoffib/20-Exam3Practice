b6d01e9c0239b32c05c9fa006432abbeabe82874
popoffib <popoffib@rose-hulman.edu>
2019-04-02 14:11:27 -0400

    done m4

--- src/m4_sequences.py
+++ src/m4_sequences.py
@@ -6,8 +6,8 @@ This problem provides practice at:
 
 Authors: David Mutchler, Vibha Alangar, Matt Boutell, Dave Fisher,
          Mark Hays, Amanda Stouder, Aaron Wilkin, their colleagues,
-         and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         and Isabella Popoff.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 ###############################################################################
 # Students:
@@ -135,13 +135,18 @@ def practice_problem4a(sequence):
       :type sequence: list | tuple | string
     """
     ###########################################################################
-    # TODO: 2. Implement and test this function.
+    # DONE: 2. Implement and test this function.
     #     The testing code is already written for you (above).
     ###########################################################################
     # DIFFICULTY AND TIME RATINGS (see top of this file for explanation)
     #    DIFFICULTY:      7
     #    TIME ESTIMATE:   15 minutes.
     ###########################################################################
+    total = []
+    for k in range(len(sequence) - 1):
+        if sequence[k] == sequence[k + 1]:
+            total = total + [k]
+    return total
 
 
 def run_test_practice_problem4b():
@@ -198,13 +203,18 @@ def practice_problem4b(sequence):
       :type sequence: (list | tuple) of (float | int)
     """
     # -------------------------------------------------------------------------
-    # TODO: 3. Implement and test this function.
+    # : 3. Implement and test this function.
     #     The testing code is already written for you (above).
     ###########################################################################
     # DIFFICULTY AND TIME RATINGS (see top of this file for explanation)
     #    DIFFICULTY:      5
     #    TIME ESTIMATE:   10 minutes.
     ###########################################################################
+    big = sequence[0]
+    for k in range(0, len(sequence), 2):
+        if sequence[k] > big:
+            big = sequence[k]
+    return big
 
 
 def run_test_practice_problem4c():
@@ -296,7 +306,7 @@ def practice_problem4c(points):
       :rtype: rg.Point | string
     """
     ###########################################################################
-    # TODO: 4. Implement and test this function.
+    # DONE: 4. Implement and test this function.
     #     The testing code is already written for you (above).
     #
     # IMPORTANT: This problem is your LOWEST PRIORITY for preparing
@@ -307,6 +317,15 @@ def practice_problem4c(points):
     #    DIFFICULTY:      9
     #    TIME ESTIMATE:   15 minutes.
     ###########################################################################
+    for k in range(len(points)):
+        if is_prime(points[k].x) is True:
+            if is_prime(points[k].y) is True:
+                y = points[k].y
+                x = points[k].x
+                points[k].x = y
+                points[k].y = x
+                return points[k]
+    return 'Not found'
 
 
 def run_test_practice_problem4d():
@@ -392,13 +411,20 @@ def practice_problem4d(sequence):
       :rtype: int
     """
     ###########################################################################
-    # TODO: 5. Implement and test this function.
+    # DONE: 5. Implement and test this function.
     #     The testing code is already written for you (above).
     ###########################################################################
     # DIFFICULTY AND TIME RATINGS (see top of this file for explanation)
     #    DIFFICULTY:      7
     #    TIME ESTIMATE:   15 minutes.
     ###########################################################################
+    total = 0
+    for k in range(len(sequence) - 1):
+        if sequence[k] != sequence[k + 1]:
+            if is_prime(sequence[k]) is True:
+                if is_prime(sequence[k + 1]):
+                    total = total + sequence[k]
+    return total
 
 
 # -----------------------------------------------------------------------------
